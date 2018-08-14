7d09b7f52efabe449488b224339d1f720b786e35
ChangsikYun <sonamu12@gmail.com>
2018-08-14 14:34:21 +0900

    Create typing_machine.cc

---
+++ New/typing_machine.cc
@@ -0,0 +1,145 @@
+// Copyright 2018 <Author>
+
+#include "typing_machine.h"
+
+TypingMachine::TypingMachine() {
+
+  this->pCursor = new Node('|');
+  this->pHead = this->pCursor;
+  this->pTail = this->pCursor;
+  this->nodeCount = 0;
+
+  return;
+}
+
+void TypingMachine::HomeKey() {
+	if (this->pHead == this->pCursor)
+		return;
+	
+	Node *nHead = this->pHead;
+	Node *nPrev = this->pCursor->GetPreviousNode();
+	
+	if (this->pTail == this->pCursor) {
+		if (this->pCursor->GetPreviousNode() != nullptr) {
+			this->pTail = this->pCursor->GetPreviousNode();
+		}
+	}
+
+	if (nPrev) {
+		nPrev->EraseNextNode();	
+	}
+
+	Node *newCursor = nHead->InsertPreviousNode('|');
+	this->pCursor = newCursor;
+	this->pHead = this->pCursor;
+
+    return;
+}
+
+void TypingMachine::EndKey() {
+	if (this->pTail == this->pCursor)
+		return;
+
+	Node *nTail = this->pTail;
+	Node *nPrev = this->pCursor->GetPreviousNode();
+
+	if (this->pHead == this->pCursor) {
+		if (this->pCursor->GetNextNode() != nullptr) {
+			this->pHead = this->pCursor->GetNextNode();
+		}
+	}
+
+	if (nPrev) {
+		nPrev->EraseNextNode();
+	}
+
+	Node *newCursor = nTail->InsertNextNode('|');
+	this->pCursor = newCursor;
+	this->pTail = this->pCursor;
+	return;
+}
+
+void TypingMachine::LeftKey() {
+
+	if (this->pHead == this->pCursor)
+		return;
+
+	Node *pNode = this->pCursor->GetPreviousNode();
+	if (this->pTail == this->pCursor)
+		this->pTail = pNode;
+	pNode->EraseNextNode();
+	
+	Node *newCursor = pNode->InsertPreviousNode ('|');
+	this->pCursor = newCursor;
+
+	if (pNode == this->pHead)
+		this->pHead = newCursor;
+
+	return;
+}
+
+void TypingMachine::RightKey() {
+	if (this->pTail == this->pCursor)
+		return;
+
+	Node *pNode = this->pCursor->GetNextNode();
+	if (this->pHead == this->pCursor)
+		this->pHead = pNode;
+	pNode->ErasePreviousNode();
+
+	Node *newCursor = pNode->InsertNextNode('|');
+	this->pCursor = newCursor;
+	
+	if (pNode == this->pTail)
+		this->pTail = newCursor;
+
+	return;
+}
+
+bool TypingMachine::TypeKey(char key) {
+
+	if (this->nodeCount >= 100 || key < 0x20 || key > 0x7E)
+		return false;	
+	else {
+		Node *node = this->pCursor->InsertPreviousNode(key);
+		if (this->pCursor == this->pHead)
+			this->pHead = node;
+		this->nodeCount++;
+		return true;
+	}
+}
+
+bool TypingMachine::EraseKey() {
+	
+	Node *pNode = this->pCursor->GetPreviousNode();
+	if (pNode) {
+		this->pCursor->ErasePreviousNode();
+		this->nodeCount--;
+		if (pNode == this->pHead)
+			this->pHead = this->pCursor;
+		return true;
+	}
+	else
+		return false;
+}
+
+std::string TypingMachine::Print(char separator) {
+	Node *pTemp = this->pHead; 
+	std::string str = "";
+	while (pTemp) {
+		
+		if (pTemp == this->pCursor) {
+			if (separator != '\0') {
+				str.push_back(separator);
+			}
+		}
+		else
+			str.push_back(pTemp->GetData());
+		
+		if (pTemp->GetNextNode() != nullptr)
+			pTemp = pTemp->GetNextNode();
+		else
+			break;
+	}
+	return str;
+}
