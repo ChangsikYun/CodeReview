1371e5b9cc5582c629967095fcc6e9521798ee87
ChangsikYun <sonamu12@gmail.com>
2018-08-14 15:33:03 +0900

    Update node.cc for resloving code review result.
    https://github.com/ChangsikYun/CodeReview/commit/7963d7cf172b1b13cbb5174d7168de51f0d7f3ad

--- New/node.cc
+++ New/node.cc
@@ -1,11 +1,8 @@
-// Copyright 2018 <Author>
+// Copyright 2018 Changsik Yun (changsik.yun@samsung.com)
 
 #include "node.h"
 
-Node::Node(char data) {
-	this->data = data;
-	this->pNext = nullptr;
-	this->pPrev = nullptr;
+Node::Node(char data) : data(data), pPrev(nullptr), pNext(nullptr){
 	return;
 }
 
@@ -14,58 +11,58 @@ char Node::GetData() {
 }
 
 Node* Node::GetPreviousNode() {
-	if (this->pPrev)
-		return this->pPrev;
-	else
-		return nullptr;
+	return this->pPrev;
 }
 
 Node* Node::GetNextNode() {
-	if (this->pNext)
-		return this->pNext;
+	return this->pNext;
+}
+
+Node* Node::InsertPreviousNode(char data) {
+    	Node *node = new Node(data);
+	if (node) {
+		Node *prev = this->pPrev;
+
+		if (prev != nullptr) {
+			prev->pNext = node;
+			node->pPrev = prev;
+		}	
+		node->pNext = this;
+		this->pPrev = node;
+
+		return node;
+	}
 	else
 		return nullptr;
 }
 
-Node* Node::InsertPreviousNode(char data) {
-	Node *node = new Node(data);
-	Node *prev = this->pPrev;
-
-	if (prev != nullptr) {
-		prev->pNext = node;
-		node->pPrev = prev;
-	}	
-	node->pNext = this;
-	this->pPrev = node;
-    
-    return node;
-}
-
 Node* Node::InsertNextNode(char data) {
-	Node *node = new Node(data);
-	Node *next = this->pNext;
+	if (node) {
+	
+		Node *node = new Node(data);
+		Node *next = this->pNext;
 
-	if (next != nullptr) {
-		next->pPrev = node;
-		node->pNext = next;
+		if (next != nullptr) {
+			next->pPrev = node;
+			node->pNext = next;
+		}
+		node->pPrev = this;
+		this->pNext = node;
+
+		return node;
 	}
-	node->pPrev = this;
-	this->pNext = node;
-
-	return node;
+	else
+		return nullptr;
 }
 
 bool Node::ErasePreviousNode() {
 	if (this->pPrev)
 	{
 		Node *node = this->pPrev;
+		Node *prev = node->pPrev;
+		this->pPrev = prev;
 		if (node->pPrev) {
-			Node *prev = node->pPrev;
 			prev->pNext = this;
-			this->pPrev = prev;
-		}
-		else {
-			this->pPrev = nullptr;
 		}
 		delete node;
 		return true;
@@ -77,13 +74,10 @@ bool Node::EraseNextNode() {
 	if (this->pNext)
 	{
 		Node *node = this->pNext;
+		Node *next = node->pNext;
+		this->pNext = next;
 		if (node->pNext) {
-			Node *next = node->pNext;
 			next->pPrev = this;
-			this->pNext = next;
-		}
-		else {
-			this->pNext = nullptr;
 		}
 		delete node;
 		return true;
