dae0c6d858a36a990018d3d2f3cc757203c37c75
ChangsikYun <sonamu12@gmail.com>
2018-08-14 14:32:25 +0900

    Create node.h

---
+++ New/node.h
@@ -0,0 +1,24 @@
+// Copyright 2018 <Author>
+
+#ifndef NODE_H_
+#define NODE_H_
+
+class Node {
+ private:
+  Node *pNext;
+  Node *pPrev; 
+  char data;
+ public:
+  explicit Node(char data);
+  char GetData();
+  Node* InsertPreviousNode(char data);
+  Node* InsertNextNode(char data);
+  Node* GetPreviousNode();
+  Node* GetNextNode();
+  bool ErasePreviousNode();
+  bool EraseNextNode();
+
+
+};
+
+#endif  // NODE_H_
